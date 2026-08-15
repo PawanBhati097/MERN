e2c33e9c7c39e05594de6ea49eb98f3126ec5308
Pawan Bhati <pawanbhati6771@gmail.com>
2026-08-16 00:27:54 +0530

    Updated this/call/apply/bind concepts with HTML demo

--- Javascript/this call apply bind/index.js
+++ Javascript/this call apply bind/index.js
@@ -1,14 +1,163 @@
 //this call apply bind
 //this : call object method
 // strict mode vs non strict mode
-'use strict'
-var a=10;
-b=20;
+//globalThis points to global object of the environment (universal use)
+//norma function:without strict mode,this wiil point to global object
+//in strict mode it will point to undefined
+//call apply bind, this apply to the invoking object
+//arrow function takes this keyword from its lexical environment scope
 
-console.log(a,b);
+// 'use strict'
+// var a=10;
+//  var b=20;
 
-function greet(name,name){
-    console.log(name,name);
-}
+// console.log(a,b);
 
-greet("Rohit","Mohit");
+// function greet(name1,name2){
+//     console.log(name1,name2);
+// }
+
+// greet("Rohit","Mohit");
+
+//console.log(window);//error//given by browser
+
+//console.log(global);//part of js
+
+//document.getElementById("h1")//document part of browser or dom
+
+
+//--------------------This keyword------------------------------
+
+//this global in global scope:NodeJS(Empty object),but in browser it will point to global object
+
+//console.log(this);
+
+//function
+
+// function greet(){
+//         console.log(`hi ${this.name}`);
+//     }
+
+//     function incrementAge(value,name){
+//         this.age+=value;
+//         this.name=name;
+//         console.log(this.age);
+//         console.log(this.name);
+
+//     }
+
+// const user={
+//     name:"rohit",
+//     age:30,
+// }
+// const user2={
+//     name:"mohit"
+// }
+// // greet.call(user);
+// // greet.call(user2);
+
+// //incrementAge.call(user,5,"mohan");
+// //------------------------------------------Apply------------------------------------------------
+
+// //incrementAge.apply(user,[5,"mohan"]);
+
+// //-----------------------------------------Bind--------------------------------------------------
+
+// const inr=incrementAge.bind(user,10,"mohan");
+
+// //console.log(inr)
+
+// inr();
+
+// //greet();
+
+// // user2.greet=user.greet;
+
+// // user2.greet();
+
+// // `use strict`
+
+// // function greet(){
+// //     console.log(this);
+// // }
+
+// // greet();
+
+
+
+
+
+
+
+
+//class
+
+// class Person{
+//     constructor(name,age){
+//         this.name=name;
+//         this.age=age;
+//     }
+// }
+// //new creates an empty object and this points to that empty object{}
+// const p1=new Person("Rohit",20)
+// console.log(p1);
+
+//Arrow Function: this doesnt exist for arrow function,lexical environment scope
+
+// const greet=()=>{
+//     console.log(this);
+// }
+
+// greet();
+
+// const user={
+//     name:"rohit",
+//     greet:function(){
+//         const that=this;
+//         function meet(){
+//             console.log(that);
+
+//         }
+//         meet();
+//     }
+// }
+// console.log(user.greet());
+
+// const stopWatch={
+//     second:0,
+//     start:function(){
+//         const that=this;
+//         setInterval(function(){
+//             that.second++;
+//             console.log(that.second);
+//         },1000);
+//     }
+// }
+
+// const stopWatch={
+//     second:0,
+//     start:function(){
+//         setInterval(()=>{
+//             this.second++;
+//             console.log(this.second);
+//         },1000)
+//     }
+// }
+
+// stopWatch.start();
+
+// const user={
+//     name:"rohit",
+//     greet:()=>{
+//         console.log(this)
+//     }
+// }
+// user.greet();
+
+
+const button=document.getElementById("first");
+
+button.addEventListener('click',function(){
+    console.log(this);
+})
+
