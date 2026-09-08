68defae278a6dbc873231b7c6bf0260614b9f8f6
Pawan Bhati <pawanbhati6771@gmail.com>
2026-09-08 22:59:48 +0530

    Removed React_1 folder, updated ReactJs with new changes

---
+++ ReactJs/DAY 1/app.js
@@ -0,0 +1,5 @@
+const a=20;
+
+ export const arr=[10,20,30,40];
+
+export default a;
---
+++ ReactJs/DAY 1/srcipt.js
@@ -0,0 +1,8 @@
+// var h1=document.createElement('h1');
+// h1.textContent="Hello from js";
+// document.body.append(h1);
+
+import a from './app.js'
+import arr from '/.app.js'
+console.log(a);
+console.log(arr);
