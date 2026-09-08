e195516deed6a47085b542ef1ab36ac548401fcb
Pawan Bhati <pawanbhati6771@gmail.com>
2026-09-09 00:00:43 +0530

    Added 02-components and 03-props, updated notex.txt

---
+++ ReactJs/02-components/src/components/Card.jsx
@@ -0,0 +1,11 @@
+export default function Cars(){
+    const user='Bhati'
+    const age=22;
+   return <>
+    <div className='card'>
+        <h1>Pawan {user}</h1>
+        <h2>Age : {age}</h2>
+      </div>
+    </>
+    
+}
---
+++ ReactJs/03-props/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Card() {
+  return (
+          <div className="card">
+       <img src="/ridho-ibrahim-4KzwdaUQF3A-unsplash.jpg" alt="Profile" />
+        <h1>Pawan Bhati</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+      <button>View Profile</button>
+      </div>
+  )
+}
+
+export default Card
